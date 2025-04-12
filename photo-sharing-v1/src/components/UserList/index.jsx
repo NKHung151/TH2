b1cb3ee2af3a2d0dabd3b2630e33c54866bf0ea2
NKHung151 <hung1512004kh@gmail.com>
2025-04-12 13:12:11 +0700

    Update index.jsx

--- photo-sharing-v1/src/components/UserList/index.jsx
+++ photo-sharing-v1/src/components/UserList/index.jsx
@@ -49,32 +49,25 @@ import {
   ListItem,
   ListItemText,
   Typography,
-} from "@mui/material"; // Import Material-UI components
-import models from "../../modelData/models"; // Import models to fetch user data
-import "./styles.css"; // Import CSS file for styling
+} from "@mui/material";
+import models from "../../modelData/models"; 
+import "./styles.css"; 
 
-/**
- * Define UserList, a React component of Project 4.
- */
 function UserList() {
-  const users = models.userListModel(); // Fetch the list of users
+  const users = models.userListModel(); 
 
   return (
     <div className="user-list">
-      {/* Description of the user list */}
+
       <Typography variant="body1" paragraph>
         This is the user list, which takes up 3/12 of the window. You might
         choose to use <a href="https://mui.com/components/lists/">Lists</a> and{" "}
         <a href="https://mui.com/components/dividers/">Dividers</a> to display
         your users like so:
       </Typography>
-
-      {/* Header for the user list */}
       <Typography variant="h4" gutterBottom>
         User List
       </Typography>
-
-      {/* Using List and ListItem from Material-UI */}
       <List component="nav">
         {users.map((user) => (
           <div key={user._id}>
@@ -82,13 +75,11 @@ function UserList() {
             <ListItem button component={Link} to={`/users/${user._id}`}>
               <ListItemText primary={`${user.first_name} ${user.last_name}`} />
             </ListItem>
-            {/* Adding a Divider between items in the list */}
+    
             <Divider />
           </div>
         ))}
       </List>
-
-      {/* Additional description about the model */}
       <Typography variant="body1" paragraph>
         The model comes in from models.userListModel()
       </Typography>
